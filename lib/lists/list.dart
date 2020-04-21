06ca1bc309dd0982cbfc87fb5eac9c4277ea85d4
luozhiran <820152235@qq.com>
2020-04-21 20:53:56 +0800

    f

---
+++ lib/lists/list.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/cupertino.dart';
+
+class SimpleList extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    final title = '简单listview';
+    return new ;
+  }
+
+}
