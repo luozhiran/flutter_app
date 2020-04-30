54343ec3dff4f2dad74e3b925fc22a3462626d07
luozhiran <820152235@qq.com>
2020-04-30 17:04:52 +0800

    开始

---
+++ lib/use/goods_category_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class GoodsCategory extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new _GoodsCategoryState();
+  }
+}
+
+class _GoodsCategoryState extends State<GoodsCategory> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
